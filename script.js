fe785f6a422e024833a2ea661e5453d0fd3df2d1
YannisLing <3270614976@qq.com>
2025-10-27 01:36:02 +0800

    半成品,我需要学习中级css以完成这个项目

--- script.js
+++ script.js
@@ -22,13 +22,36 @@ function Book(title, author, pages, isreade) {
 function addBookToLibrary(title, author, pages, isreade) {
     const newBook = new Book (title, author,pages,isreade);
     myLibrary.push(newBook);
-
-
-
 }
 
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
 
 
+function showBooks(myLibrary){
+    const parent = document.querySelector('.box2');
+    const parent2 = document.querySelector('ul')
+    for(item of myLibrary){
+        
+        const newElement = document.createElement('div');
+        const newElement2 = document.createElement('li');
+        newElement.className='card'
+        newElement.innerHTML = 
+        `
+        <div>titie: ${item.title}</div>
+        <div>author: ${item.author}</div>
+        <div>number of pages: ${item.numberOfPages}</div>
+        <div>isreade: ${item.isreade}</div>
+        <div>id: ${item.id}</div>
+        `;
+        newElement2.innerHTML=`<a href="#">⭐${item.title}</a>`
+        parent.appendChild(newElement);
+        parent2.appendChild(newElement2);
+    }
+}
+showBooks(myLibrary);
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
 console.log(theHobbit.info());
